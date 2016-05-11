f5b858e68c66d93b461d001cd28dbd60ab5b7c83
kennethwang14 <kennethwang14@gmail.com>
2016-05-10 22:58:19 -0700

    Uses metalsmith instead of nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,45 @@
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
 
+    /* Appending header */
     header  = require('gulp-header'),
     package = require('./package.json'),
 
+    /* CSS */
     sass = require('gulp-sass'),
+    sassGlob = require('gulp-sass-glob'),
     sourcemaps  = require('gulp-sourcemaps'),
     autoprefixer = require('autoprefixer'),
     cssnano = require('gulp-cssnano'),
     postcss = require('gulp-postcss'),
-    uncss = require('gulp-uncss'),
     lost = require('lost'),
 
+    /* JavaScript */
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
 
+    /* HTML */
     browserSync = require('browser-sync').create(),
-    nunjucksRender = require('gulp-nunjucks-render'),
     htmlmin = require('gulp-htmlmin'),
+    Metalsmith = require('metalsmith'),
+    gulpsmith = require('gulpsmith'),
+    layout = require('metalsmith-layouts'),
+    permalinks = require('metalsmith-permalinks'),
+    markdown = require('metalsmith-markdown-remarkable'),
+    gulp_front_matter = require('gulp-front-matter'),
+    assign = require('lodash.assign'),
 
+    /* ImageMin */
     imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    jpegoptim = require('imagemin-jpegoptim'),
     cache = require('gulp-cache'),
 
+    /* Helpers */
+    plumber = require('gulp-plumber'),
     runSequence = require('run-sequence'),
     del = require('del');
+
+    
 
 //Banner header
 var banner = [
@@ -47,6 +63,7 @@ gulp.task('css', function () {
   gulp.src('app/assets/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
       lost(),
@@ -58,20 +75,16 @@ gulp.task('css', function () {
     .pipe(gulp.dest('dist/assets/css'));
 });
 
-gulp.task('uncss', function () {
+gulp.task('css-build', function () {
   gulp.src('app/assets/scss/**/*.scss')
     .pipe(plumber())
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
-    .pipe(uncss({
-            html: ['dist/**/*.html'],
-            ignore: [/js-/]
-     }))
     .pipe(postcss([
       lost(),
       autoprefixer()
     ]))
     .pipe(cssnano())
-    .pipe(sourcemaps.write())
     .pipe(header(banner, { package : package }))
     .pipe(gulp.dest('dist/assets/css'));
 });
@@ -86,10 +99,23 @@ gulp.task('js',function(){
 });
 
 gulp.task('html', function() {
-   gulp.src(['app/**/*.html', 'app/**/*.nunjucks', '!app/templates', '!app/templates/**'])
-    .pipe(nunjucksRender({
-      path: ['app/templates']
-    }))
+   gulp.src(['app/html/*.html'])
+   .pipe(gulp_front_matter()).on("data", function(file) {
+      assign(file, file.frontMatter); 
+      delete file.frontMatter;
+    })
+    .pipe(
+      gulpsmith() 
+      .metadata({site_name: "Krav Maga Experts"})
+      .use(layout({
+        engine: "handlebars",
+        directory: "app/html/layouts",
+        partials: "app/html/partials"
+      }))
+      .use(permalinks({
+        pattern: ':root/:title'
+      }))
+    )
     .pipe(htmlmin({
       removeComments: true,
       collapseWhitespace: true,
@@ -104,11 +130,17 @@ gulp.task('html', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('images', function() {
+gulp.task('html-home', function() {
+  gulp.src("dist/home/index.html")
+  .pipe(gulp.dest("dist"));
+});
+
+gulp.task('imagemin', function() {
   return gulp.src('app/assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    .pipe(cache(imagemin({
-      interlaced: true,
-    })))
+    .pipe(imagemin({
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant({quality: "80", floyd: 1, speed: 1}), jpegoptim({progressive: true, max: 90})]
+    }))
     .pipe(gulp.dest('dist/assets/images'))
 });
 
@@ -116,10 +148,15 @@ gulp.task('clean', function() {
     return del.sync('dist');
 });
 
+
 gulp.task('copy', function() {
-   gulp.src(['app/**', '!app/templates', '!app/templates/**', 'app/.htaccess'])
-    .pipe(gulp.dest('dist'))
+   gulp.src(['app/*.*', 'app/.htaccess'])
+    .pipe(gulp.dest('dist'));
+  gulp.src(['app/assets/fonts/**'])
+    .pipe(gulp.dest('dist/assets/fonts'));
 });
+
+
 
 gulp.task('browser-sync', function() {
     browserSync.init(null, {
@@ -132,31 +169,34 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function() {
     gulp.watch("app/assets/scss/**/*.scss", ['css']).on('change', function(evt) {
-      browserSync.reload();
+    	browserSync.reload();
     });
 
     gulp.watch("app/assets/js/**/*.js", ['js']).on('change', function(evt) {
-      browserSync.reload();
+    	browserSync.reload();
     });
 
-    gulp.watch("app/**/*.+(html|nunjucks)", ['html']).on('change', function(evt) {
-      browserSync.reload();
+    gulp.watch("app/**/*.+(html)", ['html, html-home']).on('change', function(evt) {
+    	browserSync.reload();
     });
 
-    gulp.watch("app/**/*.+(png|jpg|jpeg|gif)", ['images']).on('change', function(evt) {
+    gulp.watch("app/assets/images/**/*.+(png|jpg|jpeg|gif)", ['images']).on('change', function(evt) {
       browserSync.reload();
     });
 });
 
-gulp.task('default', function(callback) {
-  runSequence(['css', 'js', 'html', 'browser-sync', 'watch'],
+gulp.task('default', function (callback) {
+  runSequence('clean', 'copy', 'html',
+    ['imagemin', 'css', 'js'],
+    'html-home', ['browser-sync', 'watch'],
     callback
   )
 });
 
 gulp.task('build', function (callback) {
-  runSequence('clean', 'copy',
-    ['images', 'uncss', 'js', 'html'],
+  runSequence('clean', 'copy', 'html',
+    ['imagemin', 'css-build', 'js'],
+    'html-home',
     callback
   )
 });
